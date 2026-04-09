4e6809fd22ff4409265b14741bdc8c48f5b55ce5
Vivek <vivekmandal123v@gmail.com>
2026-04-09 11:26:02 +0530

    fix: validator compatible grader references

--- tasks/scenarios.py
+++ tasks/scenarios.py
@@ -1,12 +1,7 @@
-from tasks.graders import (
-    grade_traffic_spike,
-    grade_database_overload,
-    grade_failed_deployment
-)
-
 easy_task = {
     "id": "traffic_spike",
     "difficulty": "easy",
+    "grader": "tasks.graders:grade_traffic_spike",
     "initial_state": {
         "incident": "traffic_spike",
         "cpu_usage": 95,
@@ -15,13 +10,13 @@ easy_task = {
         "network_errors": 5,
         "error_rate": 8,
         "servers": 3
-    },
-    "grader": grade_traffic_spike
+    }
 }
 
 medium_task = {
     "id": "database_overload",
     "difficulty": "medium",
+    "grader": "tasks.graders:grade_database_overload",
     "initial_state": {
         "incident": "database_overload",
         "cpu_usage": 85,
@@ -30,13 +25,13 @@ medium_task = {
         "network_errors": 8,
         "error_rate": 12,
         "servers": 3
-    },
-    "grader": grade_database_overload
+    }
 }
 
 hard_task = {
     "id": "failed_deployment",
     "difficulty": "hard",
+    "grader": "tasks.graders:grade_failed_deployment",
     "initial_state": {
         "incident": "failed_deployment",
         "cpu_usage": 75,
@@ -45,8 +40,7 @@ hard_task = {
         "network_errors": 10,
         "error_rate": 20,
         "servers": 2
-    },
-    "grader": grade_failed_deployment
+    }
 }
 
 TASKS = [easy_task, medium_task, hard_task]
